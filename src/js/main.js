80e5113b8713e15a78f389f61e2a2a61e3793b29
Nikita Kalachev <nikitakalachev18@gmail.com>
2017-08-01 18:38:30 +0300

    beginnig About section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 particlesJS.load('particles-js', 'js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+    //typewriter
+$('.header-welcome p').type(function() {
+	$('.header-welcome .tw-header-title').animate({opacity: 1}, 1500
+	);
+}, 70);
