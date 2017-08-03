c8d6f35884961db03c93c0c02b09fdd5ffe592c5
Nikita Kalachev <nikitakalachev18@gmail.com>
2017-08-03 16:49:11 +0300

    began of skills

--- build/js/main.js
+++ build/js/main.js
@@ -6,4 +6,18 @@ particlesJS.load('particles-js', 'js/particles.json', function() {
 $('.header-welcome p').type(function() {
 	$('.header-welcome .tw-header-title').animate({opacity: 1}, 1500
 	);
-}, 70);
+}, 2);
+
+
+
+
+$(document).ready(function() {
+	$('.box-img').viewportChecker({
+		classToAdd: 'box-img-filter',
+		offset: 400
+	});
+	$('.my-img').viewportChecker({
+		classToAdd: 'my-img-anim',
+		offset: 350
+});
+});
